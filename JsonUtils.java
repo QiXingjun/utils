59a9b0fee5b6bf5bd15795a135141c384fce810c
齐小白 <18862141550@163.com>
2017-03-21 12:43:58 +0800

    Update JsonUtils.java

--- JsonUtils.java
+++ JsonUtils.java
@@ -1,16 +1,10 @@
-package com.taotao.utils;
-
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.taotao.result.TaotaoResult;
 
-/**
- * 淘淘商城自定义响应结构
- */
 public class JsonUtils {
 
     // 定义jackson对象
