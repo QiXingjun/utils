738bb572d4637e16215927baa0214f89e8b85f47
齐小白 <18862141550@163.com>
2017-10-12 17:45:50 +0800

    Update python_utils.py

--- python_utils.py
+++ python_utils.py
@@ -6,4 +6,13 @@ def get_api_result(url, filter_params):
         return requests.get(url, params=filter_params).json()
 
 ========================================================================
-#python 保存数据为cvs文件
+#python 保存数据为csv文件
+
+import csv
+
+headers = ['xxx', 'xxx']
+with open('csv_name.csv','wb') as f:
+        f_csv = csv.DictWriter(f, headers)
+        f.write(','.join(headers))
+        f.write('\n')
+        f_csv.writerows(info_list)
