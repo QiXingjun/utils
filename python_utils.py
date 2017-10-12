a3a137214f705438ce4758139970aaff743e19e1
齐小白 <18862141550@163.com>
2017-10-12 17:41:06 +0800

    Update python_utils.py

--- python_utils.py
+++ python_utils.py
@@ -1,5 +1,9 @@
+========================================================================
 # python 通过requests请求api
 import requests
 
 def get_api_result(url, filter_params):
         return requests.get(url, params=filter_params).json()
+
+========================================================================
+#python 保存数据为cvs文件
